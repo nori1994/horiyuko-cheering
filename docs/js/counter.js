8c5952b945d7d1a2d4926c55c8a64fd9e6a64667
nori1994 <313.hy.sss@gmail.com>
2019-05-12 20:55:57 +0900

    結果条件更新

--- docs/js/counter.js
+++ docs/js/counter.js
@@ -11,12 +11,12 @@ function count_up() {
 // URLはあくまでHTMLから見た
 const result_img = new Array("./asset/desk_mashroom_increased.png", "./asset/window_ame.png", "./asset/spoon_bented_success.png");
 function change_result_img() {
-    if (counter > 2) {
+    if (counter > 60) {
         document.getElementById("spoon_img").src = result_img[2];
         document.getElementById("success_result_area").style.visibility = "visible";
         document.getElementById("success_score_area").style.visibility = "visible";
         document.getElementById("success_count").innerText = counter;
-    } else if (counter > 1) {
+    } else if (counter > 30) {
         document.getElementById("desk_img").src = result_img[0];
         document.getElementById("desk_result_area").style.visibility = "visible";
         document.getElementById("desk_score_area").style.visibility = "visible";
