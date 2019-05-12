8fe8626a81201647eea8adacab3f1de304aad5cb
nori1994 <313.hy.sss@gmail.com>
2019-05-13 00:32:45 +0900

    結果条件の変更、READDMEの修正

--- docs/js/counter.js
+++ docs/js/counter.js
@@ -8,24 +8,24 @@ function count_up() {
 }
 
 function show_result() {
-    if (counter > 58) {
+    if (counter > 80) {
         // スプーン曲げ成功
         document.getElementById("spoon_img").src = result_img[2];
         document.getElementById("success_result_area").style.visibility = "visible";
         document.getElementById("success_score_area").style.visibility = "visible";
         document.getElementById("success_count").innerText = counter;
     } else if (counter > 39) {
-        // 雨
-        document.getElementById("window_img").src = result_img[1];
-        document.getElementById("window_result_area").style.visibility = "visible";
-        document.getElementById("window_score_area").style.visibility = "visible";
-        document.getElementById("window_count").innerText = counter;
-    } else if (counter > 0) {
         // きのこ
         document.getElementById("desk_img").src = result_img[0];
         document.getElementById("desk_result_area").style.visibility = "visible";
         document.getElementById("desk_score_area").style.visibility = "visible";
         document.getElementById("desk_count").innerText = counter;
+    } else if (counter > 0) {
+        // 雨
+        document.getElementById("window_img").src = result_img[1];
+        document.getElementById("window_result_area").style.visibility = "visible";
+        document.getElementById("window_score_area").style.visibility = "visible";
+        document.getElementById("window_count").innerText = counter;
     } else {
         // 何も起こらなかった
         document.getElementById("no_pray_result_area").style.visibility = "visible";
